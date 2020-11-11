11b5a0fea01f34118223d4c58543fc770a5537c8
MilenaDimitrijevic <mdimitrijevic11419rn@raf.rs>
2020-11-11 21:19:17 +0100

    Ucitavanje custom fajla

--- SKCustomComponent/src/main/java/custom/CustomImportExport.java
+++ SKCustomComponent/src/main/java/custom/CustomImportExport.java
@@ -1,6 +1,9 @@
 package custom;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.util.HashMap;
 import java.util.List;
 
 import model.Entity;
@@ -10,8 +13,53 @@ public class CustomImportExport extends Storage {
 
 	@Override
 	public void pretraziFajl(File file) {
-		// TODO Auto-generated method stub
-		
+		try {
+			FileReader fr = new FileReader(file);
+			BufferedReader bf = new BufferedReader(fr);
+			String line = bf.readLine();
+			Entity e;
+			while(line != null) {
+				String[] split = line.split(":");
+				if(split[0].equalsIgnoreCase("_id")) {
+					e = new Entity();
+					e.setId(Integer.parseInt(split[1]));
+					line = bf.readLine();
+					split = line.split(":");
+					if(split[0].equalsIgnoreCase("naziv")) {
+						e.setNaziv(split[1].substring(0, split[1].length()-1));
+						line = bf.readLine();
+						split = line.split(":");
+					}
+					if(split[0].equalsIgnoreCase("simpleproperties")) {
+						line = bf.readLine();
+						split = line.split(":");
+						while(!split[0].equalsIgnoreCase("_id") || !split[0].equalsIgnoreCase("entityproperties")) {
+							HashMap<String, Object> simple = new HashMap<String, Object>();
+							String value = split[1];
+							if(split[1].startsWith("'")) {
+								value = split[1].substring(0, split[1].length()-1);
+							}
+							simple.put(split[0], (Object)value);
+							
+							line = bf.readLine();
+							split = line.split(":");
+						}
+					}
+					if(split[0].equalsIgnoreCase("entityproperties")) {
+						
+						
+						
+					}
+					
+				}
+				
+				line = bf.readLine();
+			}
+			bf.close();
+			fr.close();
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Override
